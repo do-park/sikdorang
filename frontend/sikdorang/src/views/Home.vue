8fbde78fedb4e2a4dd3684a7e3369921dbcf08f2
박인영 <in0_2015@naver.com>
2020-09-15 16:31:50 +0900

    UPDATE | main page transition | #S03P22D202-63

--- frontend/sikdorang/src/views/Home.vue
+++ frontend/sikdorang/src/views/Home.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="home">
-    <MainPage />
-    <LoginPage />
+    <v-btn  v-if="!nowMain" class="arrow-btn" color="primary" @click="changeToMainPage">지금은 로그인페이지</v-btn>
+    <transition name="component-fade" mode="out-in">
+      <component v-bind:is="view"></component>
+    </transition>
+    <v-btn  v-if="nowMain" class="arrow-btn" color="primary" @click="changeToLoginPage">지금은 메인페이지</v-btn>
   </div>
 </template>
 
@@ -15,6 +18,43 @@ export default {
   components: {
     MainPage,
     LoginPage
-  }
+  },
+  data() {
+    return {
+      view: MainPage,
+      nowMain: true
+    }
+  },
+  methods: {
+    changeToLoginPage() {
+      this.view = LoginPage
+      this.nowMain = false
+    },
+
+    changeToMainPage() {
+      this.view = MainPage
+      this.nowMain = true
+    }
+  },
 }
 </script>
+
+<style scope>
+  .component-fade-enter-active, .component-fade-leave-active {
+    transition: all 0.5s;
+  }
+  .component-fade-enter {
+    opacity: 0;
+    transform: translateY(30px);
+  }
+  .component-fade-leave-to {
+    opacity: 0;
+    transform: translateY(-30px);
+  }
+  .home {
+    text-align: center;
+  }
+  .arrow-btn {
+    margin: auto;
+  }
+</style>
