b62c8e18a0667410e06bce51b4b35c1e3e8036cf
허성수 <gjtjdtn201@naver.com>
2020-09-16 20:09:25 +0900

    category fix

--- backend/sikdorang/accounts/models.py
+++ backend/sikdorang/accounts/models.py
@@ -3,4 +3,5 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+    phone_number = models.CharField(max_length=20)
+    age = models.IntegerField()
--- backend/sikdorang/api/models.py
+++ backend/sikdorang/api/models.py
@@ -1,14 +1,17 @@
 from django.db import models
+from django.conf import settings
 
 class Tags(models.Model):
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=20)
+    tags_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="tags_user", blank=True)
     def __str__ (self):
         return self.name
 
 class Category(models.Model):
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=20)
+    Category_user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     def __str__ (self):
         return self.name
 
@@ -20,5 +23,5 @@ class Store(models.Model):
     address = models.CharField(max_length=200, null=True)
     latitude = models.FloatField(max_length=10, null=True)
     longitude = models.FloatField(max_length=10, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tags, related_name = 'store_tags') 
-    category = models.ManyToManyField(Category, related_name = 'store_category')
