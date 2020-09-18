9f94bfe5779a9bd284f7e68186379d94a8e19a01
허성수 <gjtjdtn201@naver.com>
2020-09-18 19:01:16 +0900

    UPDATE | DB build | #S03P22D202-55

--- backend/sikdorang/api/models.py
+++ backend/sikdorang/api/models.py
@@ -1,15 +1,23 @@
 from django.db import models
 from django.conf import settings
 
-class Tags(models.Model):
-    id = models.IntegerField(primary_key=True)
+class TagModel(models.Model):
     name = models.CharField(max_length=20)
+    count = models.IntegerField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     def __str__ (self):
         return self.name
 
 class Category(models.Model):
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=20)
+    def __str__ (self):
+        return self.name
+
+class CategoryUser(models.Model):
+    name = models.CharField(max_length=20)
+    count = models.IntegerField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     def __str__ (self):
         return self.name
 
@@ -22,4 +30,4 @@ class Store(models.Model):
     latitude = models.FloatField(max_length=10, null=True)
     longitude = models.FloatField(max_length=10, null=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    tags = models.ManyToManyField(Tags, related_name = 'store_tags') 
+    tags = models.CharField(max_length=100, null=True)
