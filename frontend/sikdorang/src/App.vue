d777d8bdab198d0e27c463ca5a776da92bc5498d
박인영 <in0_2015@naver.com>
2020-09-18 14:44:03 +0900

    UPDATE | go home after logout | #S03P22D202-118

--- frontend/sikdorang/src/App.vue
+++ frontend/sikdorang/src/App.vue
@@ -25,7 +25,9 @@ export default {
     tryLogout() {
       window.$cookies.remove('auth-token')
       this.$store.state.isLogin = false
-      
+      // 로그아웃이 완료되면 사용자를 홈페이지로 던집니다.
+			this.$router.push({ name: 'Home' })
+			window.location.reload()
 
     }
   },
