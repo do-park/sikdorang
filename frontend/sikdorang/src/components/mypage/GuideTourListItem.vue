f0dd40afd39b7787c15e291fdc72a4f0f40791d7
박인영 <in0_2015@naver.com>
2020-09-22 23:47:30 +0900

    UPDATE | mypage guide tour basic | #S03P23D202-94

---
+++ frontend/sikdorang/src/components/mypage/GuideTourListItem.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="tour" @click="onClick">
+        <div>{{ tour.title_img }}</div>
+        <div>[{{ tour.area}}]{{ tour.title }}</div>
+        <div>{{ tour.start_date }}~{{ tour.end_date }}</div>
+        <div>{{ tour.price }}원</div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "TripListItem",
+    props: {
+        tour: Object
+    },
+    methods: {
+        onClick() {
+            this.$router.push(`/trip/detail/${this.tour.id}`)
+        }
+    }
+}
+</script>
+
+<style scoped>
+.tour {
+    cursor: pointer;
+    border: 1px black solid;
+}
+</style>
--- frontend/sikdorang/src/components/mypage/MyPage.vue
+++ frontend/sikdorang/src/components/mypage/MyPage.vue
@@ -4,11 +4,13 @@
 		<UserProfile/>
 		<MyPageMap/>
 		<TripList/>
+		<GuideTourList/>
 	</div>
 </template>
 
 <script>
 import TripList from './TripList.vue'
+import GuideTourList from './GuideTourList.vue'
 import UserProfile from './UserProfile.vue'
 import MyPageMap from './MyPageMap.vue'
 
@@ -16,6 +18,7 @@ export default {
 	name: "MyPage",
 	components: {
 		TripList,
+		GuideTourList,
 		UserProfile,
 		MyPageMap,
 	},
