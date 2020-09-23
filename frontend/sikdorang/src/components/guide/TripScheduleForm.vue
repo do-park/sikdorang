f4fb6835639a4b507af33dba50295e09ab386dc5
박인영 <in0_2015@naver.com>
2020-09-23 15:04:12 +0900

    UPDATE | image url

--- frontend/sikdorang/src/components/guide/TripScheduleForm.vue
+++ frontend/sikdorang/src/components/guide/TripScheduleForm.vue
@@ -69,6 +69,7 @@ export default {
     return {
       tripSchedule: {
         title: null,
+        title_img: null,
         area: null,
         start_date: null,
         end_date: null,
@@ -91,12 +92,12 @@ export default {
           Authorization: `JWT ${this.$cookies.get("auth-token")}`,
         },
       };
-      //   if (
-      //     this.tripSchedule.title_img === null ||
-      //     this.tripSchedule.title_img === undefined
-      //   ) {
-      //     this.tripSchedule.title_img = "";
-      //   }
+        // if (
+        //   this.tripSchedule.title_img === null ||
+        //   this.tripSchedule.title_img === undefined
+        // ) {
+        //   this.tripSchedule.title_img = "";
+        // }
       console.log(this.tripSchedule);
 
       this.$axios
@@ -113,13 +114,13 @@ export default {
       let html = this.$refs.toastuiEditor.invoke("getHtml");
       this.tripSchedule.content = html;
     },
-    fileChange() {
-      // console.log("!!", e)
-      // var file = e.target.files[0]
-      // if (file && file.type.match(/^image\/(png|jpeg)$/)) {
-      //     this.tripSchedule.imageUrl = window.URL.createObjectURL(file)
-      // }
-      this.tripSchedule.title_img = this.$refs.tI.files[0];
+    fileChange(e) {
+      console.log("!!", e)
+      var file = e.target.files[0]
+      if (file && file.type.match(/^image\/(png|jpeg)$/)) {
+          this.tripSchedule.title_img = window.URL.createObjectURL(file)
+      }
+      // this.tripSchedule.title_img = this.$refs.tI.files[0];
     },
   },
 };
