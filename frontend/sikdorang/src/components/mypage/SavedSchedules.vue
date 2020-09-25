4307aac2177689f5decab2210afd73e15ab8a363
SH <seunghee6022@gmail.com>
2020-09-26 00:20:15 +0900

    UPDATE | 마이페이지 일정 저장 및 가져오기

--- frontend/sikdorang/src/components/mypage/SavedSchedules.vue
+++ frontend/sikdorang/src/components/mypage/SavedSchedules.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
+      <h3>오늘의 일정</h3>
+
       <h3>저장된 여행 일정</h3>
       <div>
           <div
@@ -21,6 +23,61 @@ export default {
         ...mapGetters("schedule",[
             "getSchedules"
         ])
+    },
+    data() {
+        return {
+            todaySchedule : [],
+            allSchedule : [],
+        }
+    },
+    methods : {
+       
+       //오늘의 스케줄과 모든 스케줄을 받아온다.
+        getTodayAndAllSchedules() {
+            const requestHeaders = {
+                headers: {
+                    Authorization: `JWT ${this.$cookies.get('auth-token')}`
+                }
+            }
+            this.$axios.get('schedules',requestHeaders)
+            .then(res=>{
+                this.todaySchedule = res.data[0]
+                this.allSchedule = res.data[1]
+            })
+            .catch(err=>{
+                console.log(err)
+            })
+        },
+
+        getTodaySchedules() {
+            const requestHeaders = {
+                headers: {
+                    Authorization: `JWT ${this.$cookies.get('auth-token')}`
+                }
+            }
+            this.$axios.get('schedules',requestHeaders)
+            .then(res=>{
+                this.todaySchedule = res.data
+            })
+            .catch(err=>{
+                console.log(err)
+            })
+        },
+        getAllSchedules() {
+            const requestHeaders = {
+                headers: {
+                    Authorization: `JWT ${this.$cookies.get('auth-token')}`
+                }
+            }
+            this.$axios.get('schedules',requestHeaders)
+            .then(res=>{
+                this.allSchedule = res.data
+            })
+            .catch(err=>{
+                console.log(err)
+            })
+        },
+     
     },
 }
 </script>
