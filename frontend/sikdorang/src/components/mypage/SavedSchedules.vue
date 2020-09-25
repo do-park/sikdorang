7b860b85d0e66e2e2d0d427b9c93c88ea543430e
SH <seunghee6022@gmail.com>
2020-09-25 11:30:39 +0900

    UPDATE | MapCard 버튼 UI 개선 | #S03P23D202-142

---
+++ frontend/sikdorang/src/components/mypage/SavedSchedules.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+      <h3>저장된 여행 일정</h3>
+      <div class="d-flex">
+          <div
+          v-for="schedule in getSchedules"
+          :key="schedule.index"
+          >
+            {{schedule.userChoice.name}} -->
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex"
+
+export default {
+    name : "SavedSchedules",
+    computed : {
+        ...mapGetters("schedule",[
+            "getSchedules"
+        ])
+    },
+}
+</script>
+
+<style>
+
+</style>
