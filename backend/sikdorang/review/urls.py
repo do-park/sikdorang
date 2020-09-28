35d622200000324300ccc7fd03b8378eeb0ebcaf
허성수 <gjtjdtn201@naver.com>
2020-09-28 10:48:57 +0900

    UPDATE | Review function

--- backend/sikdorang/review/urls.py
+++ backend/sikdorang/review/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = 'review'
 
 urlpatterns = [
+    path('create_review/<int:store_pk>', views.create_review),
+    path('user_review', views.user_review),
+    path('store_review/<int:store_pk>', views.store_review),
 
 ]
