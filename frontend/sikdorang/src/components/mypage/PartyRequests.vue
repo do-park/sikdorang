31610ce938fd9d0c7a8c1bd6e9623fe1826096c9
박인영 <in0_2015@naver.com>
2020-09-29 04:53:52 +0900

    UPDATE | html modal | #S03P23D202-330

--- frontend/sikdorang/src/components/mypage/PartyRequests.vue
+++ frontend/sikdorang/src/components/mypage/PartyRequests.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    나에게 동행 요청한 사람들과 메시지
     <div v-for="request in requestData" :key="request.party_id">
       <div>보낸사람: </div>
       <div>수신날짜: {{ request.created_at}}</div>
       <div>내용: 
         <viewer v-if="request.content" :initialValue="request.content"/>
       </div>
+      <hr>
     </div>
   </div>
 </template>
@@ -20,9 +20,11 @@ export default {
   components: {
     viewer: Viewer,
   },
+  props: {
+    partyPk: String
+  },
   data() {
     return {
-      partyPk: 1, // 가져와야함
       requestData: []
     };
   },
