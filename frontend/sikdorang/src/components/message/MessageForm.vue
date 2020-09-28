9162affddfe17d558cbbc491efcc0515fe1f3c56
박인영 <in0_2015@naver.com>
2020-09-29 04:00:13 +0900

    UPDATE | made modal | #S03P23D202-321

--- frontend/sikdorang/src/components/message/MessageForm.vue
+++ frontend/sikdorang/src/components/message/MessageForm.vue
@@ -1,14 +1,37 @@
 <template>
   <div>
-    <editor
-      ref="toastuiEditor"
-      :initialValue="editorText"
-      :options="editorOptions"
-      height="500px"
-      initialEditType="wysiwyg"
-      previewStyle="vertical"
-    />
-    <button class="btn btn-primary" @click="onClick()">생성</button>
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#partyRequestModal">동행신청 테스트 버튼</button>
+
+
+    <!-- Modal -->
+    <div class="modal fade" id="partyRequestModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">동행 신청하기</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+
+            <editor
+              ref="toastuiEditor"
+              :initialValue="editorText"
+              :options="editorOptions"
+              height="500px"
+              initialEditType="wysiwyg"
+              previewStyle="vertical"
+            />
+
+          </div>
+          <div class="modal-footer">
+            <button class="btn btn-primary" @click="onClick()">보내기</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
