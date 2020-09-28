cb5074b2ec94883847632e0683dd2c13569607a2
박인영 <in0_2015@naver.com>
2020-09-29 04:23:13 +0900

    UPDATE | connected with back | #S03P23D202-331

--- frontend/sikdorang/src/components/mypage/PartyRequests.vue
+++ frontend/sikdorang/src/components/mypage/PartyRequests.vue
@@ -4,15 +4,22 @@
     <div v-for="request in requestData" :key="request.party_id">
       <div>보낸사람: </div>
       <div>수신날짜: {{ request.created_at}}</div>
-      <div>내용: {{ request.content }}</div>
+      <div>내용: 
+        <viewer v-if="request.content" :initialValue="request.content"/>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import '@toast-ui/editor/dist/toastui-editor-viewer.css'
+import { Viewer } from '@toast-ui/vue-editor'
 
 export default {
   name: "PartyRequests",
+  components: {
+    viewer: Viewer,
+  },
   data() {
     return {
       partyPk: 1, // 가져와야함
@@ -27,6 +34,7 @@ export default {
       this.$axios
       .get(`/party/list_message/${this.partyPk}`)
       .then((res) => {
+        console.log('party~~', res.data)
         this.requestData = res.data;
       })
       .catch((err) => {
