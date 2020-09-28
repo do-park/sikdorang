4dcb389d0d3ad19a93f04886354cef2088291358
허성수 <gjtjdtn201@naver.com>
2020-09-29 01:03:00 +0900

    UPDATE | party message | #

--- backend/sikdorang/party/models.py
+++ backend/sikdorang/party/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.conf import settings
+from trip.models import Trip
 
 # Create your models here.
+
+class Party(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    trip_id = models.ForeignKey(Trip, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class PartyMessage(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    party_id = models.ForeignKey(Party, on_delete=models.CASCADE)
+    content = models.TextField()
