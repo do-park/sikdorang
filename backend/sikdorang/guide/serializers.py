383ef19b97048b1e4b1f8f1e5b995b858c9f6b65
허성수 <gjtjdtn201@naver.com>
2020-09-29 14:36:51 +0900

    UPDATE | departure person

--- backend/sikdorang/guide/serializers.py
+++ backend/sikdorang/guide/serializers.py
@@ -13,15 +13,15 @@ class GuideItemSerializer(serializers.ModelSerializer):
     # title_img = serializers.ImageField(use_url=True)
     class Meta: 
         model = TripItemModel
-        fields = ['title_img', 'title', 'area', 'start_date', 'end_date', 'price', 'start_point', 'start_time', 'content', 'limit_person']
+        fields = ['title_img', 'title', 'area', 'start_date', 'end_date', 'price', 'start_point', 'start_time', 'content', 'limit_person', 'departure_person']
 
 class GuideSerializer(serializers.ModelSerializer):
     class Meta: 
         model = TripItemModel
         depth = 1
-        fields = ['id', 'user', 'title_img', 'title', 'area', 'start_date', 'end_date', 'price', 'start_point', 'start_time', 'limit_person']
+        fields = ['id', 'user', 'title_img', 'title', 'area', 'start_date', 'end_date', 'price', 'start_point', 'start_time', 'limit_person', 'departure_person']
 
 class TourSerializer(serializers.ModelSerializer):
     class Meta: 
         model = TripItemModel
-        fields = ['id', 'title_img', 'title', 'area', 'start_date', 'end_date', 'price', 'limit_person']
+        fields = ['id', 'title_img', 'title', 'area', 'start_date', 'end_date', 'price', 'limit_person', 'departure_person']
