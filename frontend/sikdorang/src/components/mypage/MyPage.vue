0f6004e1ae3c96c2d461d557aceb33ce180cfdc5
박인영 <in0_2015@naver.com>
2020-10-01 17:59:38 +0900

    UPDATE | mypage menu design

--- frontend/sikdorang/src/components/mypage/MyPage.vue
+++ frontend/sikdorang/src/components/mypage/MyPage.vue
@@ -4,17 +4,27 @@
       <router-link to="#" class="be-guide">가이드 신청</router-link>
 		</div>
 		<UserProfile/>
-		<hr>
-		<MyPageMap/>
 
-		<div class="">
-      <div class="btn-wrap">
-        <button class="todayBtn mpBtn selected" @click="onTripList">오늘의 일정</button>
-        <button class="savedSchedulesBtn mpBtn" @click="onSavedSchedules">모든 일정</button>
-        <button class="guideTourBtn mpBtn" @click="onGuideTourList">가이드 투어</button>
-        <button class="reviewBtn mpBtn" @click="onReviewList">작성한 리뷰</button>
+		<div class="mt-5">
+      <div class="row m-0">
+        <button class="col-3 todayBtn mpBtn selected" @click="onTripList">
+          <i class="fas fa-bullseye fa-2x"></i>
+          <div class="menu-font">오늘의 일정</div>
+        </button>
+        <button class="col-3 savedSchedulesBtn mpBtn" @click="onSavedSchedules">
+          <i class="far fa-calendar-alt fa-2x"></i>
+          <div class="menu-font">모든 일정</div>
+        </button>
+        <button class="col-3 guideTourBtn mpBtn" @click="onGuideTourList">
+          <i class="fas fa-suitcase-rolling fa-2x"></i>
+          <div class="menu-font">마이 투어</div>
+        </button>
+        <button class="col-3 reviewBtn mpBtn" @click="onReviewList">
+          <i class="fas fa-keyboard fa-2x"></i>
+          <div class="menu-font">작성 리뷰</div>
+        </button>
       </div>
-      <div id="schedule" class="schedule ptBorder">
+      <div id="schedule" class="schedule">
 				<SavedSchedules :savedSchedules="savedSchedules" />
 				<TripList :tripList="tripList" />
 				<GuideTourList :guideTourList="guideTourList" />
@@ -22,6 +32,7 @@
 
       </div>
     </div>
+    <MyPageMap/>
 	</div>
 </template>
 
@@ -109,28 +120,24 @@ export default {
 }
 .schedule {
 
-    /* min-height: 500px; */
-    background-color: rgba(255, 255, 255, 0.7);
-    border-radius: 0px 30px 30px 30px;
+    min-height: 200px;
 }
 
 .mpBtn {
-	background-color: gray;
-    color: white;
-    border: none;
-    outline: none !important;
-    width: 100px;
-    height: 50px;
+  border: none;
+  outline: none !important;
+  height: 100%;
+  padding: 0px 0px 5px;
+}
+.menu-icon {
+  font-size: 15px;
+}
+.menu-font {
+  font-size: 12px;
 }
 .selected {
-	background-color: rgba(143, 160, 242, 1);
-}
-.mpBtn:hover {
-    background-color: rgba(143, 160, 242, 0.8);
+  border-bottom: 3px solid crimson;
 }
 
-.btn-wrap {
-    text-align: left;
-}
 
 </style>
