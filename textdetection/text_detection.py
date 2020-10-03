cdf5a5068bcad88de41d6a9d13877cf61ff352ba
SH <seunghee6022@gmail.com>
2020-10-03 16:21:13 +0900

    UPDATE | text_detection codes and image files

--- textdetection/text_detection.py
+++ textdetection/text_detection.py
@@ -2,7 +2,7 @@ from PIL import Image
 import pytesseract
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-image = Image.open(r'C:\Users\multicampus\Desktop\s03p23d202\textdetection\모퉁이간판.jpg')
+image = Image.open(r'C:\Users\multicampus\Desktop\s03p23d202\textdetection\rian1.jpg')
 # path1 = os.path.normpath("my_path/this_way")
 text = pytesseract.image_to_string(image,lang='kor')
 print(text)
