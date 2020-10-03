4ab6cafb7ffee80a23983636bca68db3cbe30d4f
허성수 <gjtjdtn201@naver.com>
2020-10-03 20:19:24 +0900

    HOTFIX | update profile image fix

--- backend/sikdorang/accounts/models.py
+++ backend/sikdorang/accounts/models.py
@@ -8,7 +8,7 @@ class User(AbstractUser):
     first_name = None
     last_name = None
     user_code = models.IntegerField(default=0)
-    profile_image = models.ImageField('프로필사진', default='/media/profile/default.jpg', upload_to="profile/")
+    profile_image = models.ImageField('프로필사진', default='/profile/default.jpg', upload_to="profile/")
     phone_number = models.CharField('휴대폰번호', blank=True, max_length=20)
     age = models.CharField('나이', blank=True, max_length=20)
     done_cup = models.BooleanField(default=False)
