e4a31076b212b18ee0d4f470a090069bee3193fa
허성수 <gjtjdtn201@naver.com>
2020-10-05 05:16:01 +0900

    UPDATE | truck

--- frontend/sikdorang/src/components/Map/SelectStart.vue
+++ frontend/sikdorang/src/components/Map/SelectStart.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="text-center">
       <hr />
-      <div class="top-place">대충 뭐 들어가는곳</div>
+      <div class="top-place"><truck /></div>
       <hr />
       <div>
         <button
@@ -17,9 +17,22 @@
         <!-- <button class="btn btn-primary m-3 go-btn" @click="search">
           다른 지역으로 갈래요!
         </button> -->
-        <input type="button" @click="sample3_execDaumPostcode()" value="다른 지역으로 갈래요!"><br>
-        <div id="wrap" style="display:none;border:1px solid;width:500px;height:300px;margin:5px 0;position:relative">
-          <img src="//t1.daumcdn.net/postcode/resource/images/close.png" id="btnFoldWrap" style="cursor:pointer;position:absolute;right:0px;top:-1px;z-index:1" @click="foldDaumPostcode()" alt="접기 버튼">
+        <input
+          type="button"
+          @click="sample3_execDaumPostcode()"
+          value="다른 지역으로 갈래요!"
+        /><br />
+        <div
+          id="wrap"
+          style="display:none;border:1px solid;width:500px;height:300px;margin:5px 0;position:relative"
+        >
+          <img
+            src="//t1.daumcdn.net/postcode/resource/images/close.png"
+            id="btnFoldWrap"
+            style="cursor:pointer;position:absolute;right:0px;top:-1px;z-index:1"
+            @click="foldDaumPostcode()"
+            alt="접기 버튼"
+          />
         </div>
       </div>
       <hr />
@@ -29,9 +42,13 @@
 
 <script>
 import Swal from "sweetalert2";
+import truck from "./truck.vue";
 
 export default {
   name: "SelectStart",
+  components: {
+    truck,
+  },
   data() {
     return {
       Latitude: null,
@@ -43,85 +60,91 @@ export default {
     };
   },
   mounted() {
-    const script = document.createElement('script'); 
-    /* global daum */ // 지우면 작동안함
-    script.src = "https://t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js";
+    const script = document.createElement("script"); // 지우면 작동안함
+    /* global daum */ script.src =
+      "https://t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js";
     document.head.appendChild(script);
-    this.element_wrap = document.getElementById('wrap');
+    this.element_wrap = document.getElementById("wrap");
   },
   methods: {
     foldDaumPostcode() {
-        // iframe을 넣은 element를 안보이게 한다.
-        this.element_wrap.style.display = 'none';
+      // iframe을 넣은 element를 안보이게 한다.
+      this.element_wrap.style.display = "none";
     },
-     sample3_execDaumPostcode() {
-       const self = this
-        // 현재 scroll 위치를 저장해놓는다.
-        var currentScroll = Math.max(document.body.scrollTop, document.documentElement.scrollTop);
-        new daum.Postcode({
-            oncomplete: function(data) {
-                // 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
+    sample3_execDaumPostcode() {
+      const self = this;
+      // 현재 scroll 위치를 저장해놓는다.
+      var currentScroll = Math.max(
+        document.body.scrollTop,
+        document.documentElement.scrollTop
+      );
+      new daum.Postcode({
+        oncomplete: function(data) {
+          // 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
 
-                // 각 주소의 노출 규칙에 따라 주소를 조합한다.
-                // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
-                var addr = ''; // 주소 변수
-                var extraAddr = ''; // 참고항목 변수
+          // 각 주소의 노출 규칙에 따라 주소를 조합한다.
+          // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
+          var addr = ""; // 주소 변수
+          var extraAddr = ""; // 참고항목 변수
 
-                //사용자가 선택한 주소 타입에 따라 해당 주소 값을 가져온다.
-                if (data.userSelectedType === 'R') { // 사용자가 도로명 주소를 선택했을 경우
-                    addr = data.roadAddress;
-                } else { // 사용자가 지번 주소를 선택했을 경우(J)
-                    addr = data.jibunAddress;
-                }
+          //사용자가 선택한 주소 타입에 따라 해당 주소 값을 가져온다.
+          if (data.userSelectedType === "R") {
+            // 사용자가 도로명 주소를 선택했을 경우
+            addr = data.roadAddress;
+          } else {
+            // 사용자가 지번 주소를 선택했을 경우(J)
+            addr = data.jibunAddress;
+          }
 
-                // 사용자가 선택한 주소가 도로명 타입일때 참고항목을 조합한다.
-                if(data.userSelectedType === 'R'){
-                    // 법정동명이 있을 경우 추가한다. (법정리는 제외)
-                    // 법정동의 경우 마지막 문자가 "동/로/가"로 끝난다.
-                    if(data.bname !== '' && /[동|로|가]$/g.test(data.bname)){
-                        extraAddr += data.bname;
-                    }
-                    // 건물명이 있고, 공동주택일 경우 추가한다.
-                    if(data.buildingName !== '' && data.apartment === 'Y'){
-                        extraAddr += (extraAddr !== '' ? ', ' + data.buildingName : data.buildingName);
-                    }
-                    // 표시할 참고항목이 있을 경우, 괄호까지 추가한 최종 문자열을 만든다.
-                    if(extraAddr !== ''){
-                        extraAddr = ' (' + extraAddr + ')';
-                    }
-                    // 조합된 참고항목을 해당 필드에 넣는다.
-                } 
+          // 사용자가 선택한 주소가 도로명 타입일때 참고항목을 조합한다.
+          if (data.userSelectedType === "R") {
+            // 법정동명이 있을 경우 추가한다. (법정리는 제외)
+            // 법정동의 경우 마지막 문자가 "동/로/가"로 끝난다.
+            if (data.bname !== "" && /[동|로|가]$/g.test(data.bname)) {
+              extraAddr += data.bname;
+            }
+            // 건물명이 있고, 공동주택일 경우 추가한다.
+            if (data.buildingName !== "" && data.apartment === "Y") {
+              extraAddr +=
+                extraAddr !== "" ? ", " + data.buildingName : data.buildingName;
+            }
+            // 표시할 참고항목이 있을 경우, 괄호까지 추가한 최종 문자열을 만든다.
+            if (extraAddr !== "") {
+              extraAddr = " (" + extraAddr + ")";
+            }
+            // 조합된 참고항목을 해당 필드에 넣는다.
+          }
 
-                // 우편번호와 주소 정보를 해당 필드에 넣는다.
-                Swal.fire({
-                  html: `${addr}` + "에서 시작해볼까요?",
-                }).then((res) => {
-                  if (res) {
-                    console.log(res, "ok눌렀다");
-                    self.$cookies.set("searchMethod", "Regions");
-                    self.$cookies.set("destination", addr);
-                    self.destination = "";
-                    self.$emit("flag", false);
-                  }
-                });
+          // 우편번호와 주소 정보를 해당 필드에 넣는다.
+          Swal.fire({
+            html: `${addr}` + "에서 시작해볼까요?",
+          }).then((res) => {
+            if (res) {
+              console.log(res, "ok눌렀다");
+              self.$cookies.set("searchMethod", "Regions");
+              self.$cookies.set("destination", addr);
+              self.destination = "";
+              self.$emit("flag", false);
+            }
+          });
 
-                // iframe을 넣은 element를 안보이게 한다.
-                // (autoClose:false 기능을 이용한다면, 아래 코드를 제거해야 화면에서 사라지지 않는다.)
-                self.element_wrap.style.display = 'none';
+          // iframe을 넣은 element를 안보이게 한다.
+          // (autoClose:false 기능을 이용한다면, 아래 코드를 제거해야 화면에서 사라지지 않는다.)
+          self.element_wrap.style.display = "none";
 
-                // 우편번호 찾기 화면이 보이기 이전으로 scroll 위치를 되돌린다.
-                document.body.scrollTop = currentScroll;
-            },
-            // 우편번호 찾기 화면 크기가 조정되었을때 실행할 코드를 작성하는 부분. iframe을 넣은 element의 높이값을 조정한다.
-            onresize : function(size) {
-                self.element_wrap.style.height = size.height+'px';
-            },
-            width : '100%',
-            height : '100%'
-        }).embed(this.element_wrap);
+          // 우편번호 찾기 화면이 보이기 이전으로 scroll 위치를 되돌린다.
+          document.body.scrollTop = currentScroll;
+        },
+        // 우편번호 찾기 화면 크기가 조정되었을때 실행할 코드를 작성하는 부분. iframe을 넣은 element의 높이값을 조정한다.
+        onresize: function(size) {
+          self.element_wrap.style.height = size.height + "px";
+        },
+        width: "100%",
+        height: "100%",
+      }).embed(this.element_wrap);
 
-        // iframe을 넣은 element를 보이게 한다.
-        this.element_wrap.style.display = 'block';
+      // iframe을 넣은 element를 보이게 한다.
+      this.element_wrap.style.display = "block";
     },
     search() {
       Swal.fire({
@@ -143,7 +166,7 @@ export default {
       if ("geolocation" in navigator) {
         //위치 요청
         navigator.geolocation.getCurrentPosition(
-          function (pos) {
+          function(pos) {
             this.Latitude = pos.coords.latitude;
             this.Longitude = pos.coords.longitude;
             Swal.fire(
