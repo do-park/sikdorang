27517d4c4ea8465f7bc9af68b5605b12d1e090b5
SH <seunghee6022@gmail.com>
2020-10-05 06:46:07 +0900

    MERGE with develop

--- backend/sikdorang/guide/urls.py
+++ backend/sikdorang/guide/urls.py
@@ -10,9 +10,8 @@ urlpatterns = [
     path('list', views.list_guide),
     path('paid/<int:trip_pk>', views.paid),
     path('paidtour', views.paidtour),
-    # path('paider/<int:trip_pk>', views.paider),
-    path('update/<int:trip_pk>', views.update_guide),
+    path('paider/<int:trip_pk>', views.paider),
+    # path('update/<int:trip_pk>', views.update_guide),
     path('delete/<int:trip_pk>', views.delete_guide),
-    
-    # path('guidetour/<int:tour_pk>', views.guidetour),
+
 ]
